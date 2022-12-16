7aa6b3ad814f5dc18ac1e607513b4289d7980861
Maxim Ryabykh <ryabykhms@gmail.com>
2022-12-16 11:14:12 +0300

    feat: implemented message handling and exit message display

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const getUsernameFromArgs = (args) => {
   const usefulArgs = args.slice(2);
   const usernameArg = usefulArgs.find((arg) => arg.startsWith("--username="));
@@ -11,9 +14,29 @@ const getUsernameFromArgs = (args) => {
   return username;
 };
 
-const main = () => {
+const main = async () => {
   const username = getUsernameFromArgs(process.argv);
   console.log(`Welcome to the File Manager, ${username}!`);
+
+  const streamTransform = new Transform({
+    transform: (chunk, encoding, callback) => {
+      const chunkString = chunk.toString().trim();
+
+      if (chunkString === ".exit") {
+        process.emit("SIGINT");
+      }
+
+      callback(null, `${chunkString}\n`);
+    },
+  });
+
+  await pipeline(process.stdin, streamTransform, process.stdout);
+
+  process.on("SIGINT", function () {
+    process.stdin.resume();
+    console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+    process.exit(0);
+  });
 };
 
 main();
