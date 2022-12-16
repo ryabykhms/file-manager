a8c6a9a3bd8e62e6b29e241319167b51070ca583
Maxim Ryabykh <ryabykhms@gmail.com>
2022-12-16 09:27:11 +0300

    feat: implemented welcome message

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("This is file manager!");
+const getUsernameFromArgs = (args) => {
+  const usefulArgs = args.slice(2);
+  const usernameArg = usefulArgs.find((arg) => arg.startsWith("--username="));
+
+  if (!usernameArg) {
+    throw new Error("Please enter username like --username=Vasya");
+  }
+
+  const username = usernameArg.split("--username=")[1];
+
+  return username;
+};
+
+const main = () => {
+  const username = getUsernameFromArgs(process.argv);
+  console.log(`Welcome to the File Manager, ${username}!`);
+};
+
+main();
